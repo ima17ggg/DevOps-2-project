f9cce79af05417cd5308004f0c567dbb2815748b
Teto <166560172+ima17ggg@users.noreply.github.com>
2026-09-14 22:22:39 -0600

    fix: host local and public

--- hackatec2026-frontend/vite.config.js
+++ hackatec2026-frontend/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
     server: {
         port: 64190,
+        host: true, // 👈 escucha en todas las interfaces, no solo localhost
         proxy: {
             '/api': {
                 target: process.env.VITE_API_URL || 'http://127.0.0.1:3000',
